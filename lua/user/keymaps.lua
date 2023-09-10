4cd41f8165c322bf88c21bdfd57cfc2b1401bdf0
Jatin <er.jatin2112@gmail.com>
2023-09-11 03:47:27 +0530

    relative numbering and some keymaps changes

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -28,10 +28,10 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<leader><Up>", ":resize +2<CR>", opts)
+keymap("n", "<leader><Down>", ":resize -2<CR>", opts)
+keymap("n", "<leader><Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<leader><Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
