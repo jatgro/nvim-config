a2db0c8853411078ae211e5ad9ecc40cf7131e1f
Jatin grover <akaza@Jatins-MacBook-Air.local>
2024-12-22 17:58:46 +0530

    added keymaps and a tmux navigator plugin

--- lua/jatgro/core/keymaps.lua
+++ lua/jatgro/core/keymaps.lua
@@ -29,3 +29,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "Close current tab "}
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {desc = "Go to next tab"}) -- go to next tab 
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", {desc = "Go to previous tab"}) -- go to previous tab 
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {desc =  "Open the current buffer in new tab"}) -- move the current buffer to a new tab
+
+
+keymap.set("n", "<leader>e", "<cmd>Explore<CR>", {desc =  "Open the current buffer in new tab"}) -- open the explorer 
