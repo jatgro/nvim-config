5e7d3c58bf8c5f3164cddd843207bf4144b05257
Jatin grover <akaza@Jatins-MacBook-Air.local>
2025-01-04 15:20:01 +0530

    add plugin to support TODO comments

--- lua/jatgro/plugins/telescope.lua
+++ lua/jatgro/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
     "nvim-lua/plenary.nvim", 
     {"nvim-telescope/telescope-fzf-native.nvim", build = "make" }, 
     "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
   }, 
   config = function()
     local telescope = require("telescope")
@@ -32,7 +33,7 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files " })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", {desc = "Find string in cwd"}) 
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", {desc = "Find string under the cursor in the cwd"}) 
-
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", {desc = "Find Todos"}) 
 
   end,
 }
