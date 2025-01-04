9af21cea1bd3e354c887f1b54565d024413683b9
Jatin grover <akaza@Jatins-MacBook-Air.local>
2025-01-04 15:50:23 +0530

    added plugin to ease the substitution

---
+++ lua/jatgro/plugins/substitute.lua
@@ -0,0 +1,18 @@
+return {
+  "gbprod/substitute.nvim",
+  event = {"BufReadPre", "BufNewFile"},
+  config = function()
+    local substitute = require("substitute")
+    
+    substitute.setup()
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n","s", substitute.operator, {desc ="substitute with motion"})
+    keymap.set("n","ss", substitute.line, {desc ="substitute line"})
+    keymap.set("n","S", substitute.eol, {desc ="substitute to the end of the line "})
+    keymap.set("x","s", substitute.visual, {desc ="substitute in the visual mode"})
+
+  end
+}
