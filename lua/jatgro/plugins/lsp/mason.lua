1d9e69167fbe1eb33d5d370ec2cfcd4db300fec3
jgrover6_uhg <grover_jatin@optum.com>
2025-02-09 14:57:51 +0530

    added lsp and trouble plugins and setup

--- lua/jatgro/plugins/lsp/mason.lua
+++ lua/jatgro/plugins/lsp/mason.lua
@@ -1,16 +1,16 @@
 return {
   "williamboman/mason.nvim",
-  dependencies ={
+  dependencies = {
     "williamboman/mason-lspconfig.nvim",
-  }
+  },
   config = function()
     -- import mason
-    import mason = require("mason")
+    local mason = require("mason")
 
     -- import mason-lspconfig
-    import mason_lspconfig = require("mason-lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
 
- -- enable mason and configure icons
+    -- enable mason and configure icons
     mason.setup({
       ui = {
         icons = {
@@ -20,6 +20,25 @@ return {
         },
       },
     })
+
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed ={
+        "ts_ls",
+        "html", 
+        "htmx",
+        "cssls",
+        "tailwindcss",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "marksman",
+--        "java_language_server",
+        "pyright",
+        "sqlls",
+      }
+
+     })
 
 
   end,
