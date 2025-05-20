a065b83864c7d2a7dc2e4b86fc0768e7c944cd9a
jgrover6_uhg <grover_jatin@optum.com>
2025-05-20 15:37:19 +0530

    added copilot

--- lua/jatgro/plugins/treesitter.lua
+++ lua/jatgro/plugins/treesitter.lua
@@ -1,60 +1,65 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = {"BufReadPre",  "BufNewFile"},
+  event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
-  }, 
+  },
   config = function()
     -- import nvim-treesitter plugin
     local treesitter = require("nvim-treesitter.configs")
+
+    -- enable folding with Treesitter
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt.foldenable = false -- disable folding by default
 
     -- configure treesitter
     treesitter.setup({ -- enable syntax highlighting
       highlight = {
         enable = true,
-      }, 
+      },
       -- enable indentation
-      indent = {enable = true}, 
+      indent = { enable = true },
       -- enable autotagging (w/ nvim-ts-autotag plugin)
       autotag = {
-        enable = true
+        enable = true,
       },
       -- ensure these language parsers are installed
       ensure_installed = {
-        "json", 
-        "javascript", 
-        "typescript", 
-        "tsx", 
-        "yaml", 
-        "html", 
-        "css", 
-        "prisma", 
-        "markdown", 
-        "markdown_inline", 
-        "graphql", 
-        "svelte", 
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "graphql",
+        "svelte",
         "bash",
-        "lua", 
-        "vim", 
-        "dockerfile", 
+        "lua",
+        "vim",
+        "dockerfile",
         "gitignore",
-        "query", 
-        "vimdoc", 
-        "java", 
-        "python", 
+        "query",
+        "vimdoc",
+        "java",
+        "python",
         "sql",
-        "xml"
-      }, 
-      incremental_selection= { 
-        enable = true, 
+        "xml",
+      },
+      incremental_selection = {
+        enable = true,
         keymaps = {
-          init_selection = "<C-space>", 
-          node_incremental = "<C-space>", 
-          scope_incremental = false, 
-          node_decremental = "<bs>"
-        }
-      }
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
     })
-  end
+  end,
 }
