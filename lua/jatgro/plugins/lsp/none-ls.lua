37540569113ca5c514db1c8a2ee3f7c4f9b5f329
Jatin grover <akaza@Jatins-MacBook-Air.local>
2025-05-25 20:24:03 +0530

    fixed the issue

--- lua/jatgro/plugins/lsp/none-ls.lua
+++ lua/jatgro/plugins/lsp/none-ls.lua
@@ -22,15 +22,15 @@ return {
         none_ls.builtins.diagnostics.eslint_d,
         none_ls.builtins.code_actions.eslint_d,
         none_ls.builtins.formatting.prettier.with({
-          filetypes = { 
-            "javascript", 
-            "typescript", 
-            "svelte", 
-            "graphql", 
-            "html", 
-            "css", 
-            "scss", 
-            "less" 
+          filetypes = {
+            "javascript",
+            "typescript",
+            "svelte",
+            "graphql",
+            "html",
+            "css",
+            "scss",
+            "less",
           },
         }),
 
@@ -40,25 +40,25 @@ return {
         -- Shell/Bash (common companion to other langs)
         none_ls.builtins.diagnostics.shellcheck,
         none_ls.builtins.formatting.shfmt,
-      ],
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({
-                bufnr = bufnr,
-                filter = function(client)
-                  -- Prefer none-ls for formatting where available
-                  return client.name == "none-ls"
-                end
-              })
-            end,
-          })
-        end
-      end,
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({
+                  bufnr = bufnr,
+                  filter = function(client)
+                    -- Prefer none-ls for formatting where available
+                    return client.name == "none-ls"
+                  end,
+                })
+              end,
+            })
+          end
+        end,
+      },
     })
-  end
+  end,
 }
